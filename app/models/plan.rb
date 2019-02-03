7a6594ee2c9576c33c36eb85bf52abbeeaf28c5e
Ishak <36705365+ishakbhn@users.noreply.github.com>
2019-02-03 15:48:28 +0800

    Update plan.rb

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,13 +1,6 @@
 class Plan < ApplicationRecord
-<<<<<<< HEAD
   has_and_belongs_to_many :meal
   has_and_belongs_to_many :breakfast
   has_and_belongs_to_many :lunch
   has_and_belongs_to_many :dinner
-=======
-  belongs_to :meal
-  belongs_to :food
-  belongs_to :food
-  belongs_to :food
->>>>>>> master
 end
